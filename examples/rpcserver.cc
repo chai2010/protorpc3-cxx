2a830fc0b5aab8707c566fa5881302307e851712
chai2010 <chaishushan@gmail.com>
2016-02-29 14:19:46 +0800

    use tab for indent

--- examples/rpcserver.cc
+++ examples/rpcserver.cc
@@ -11,91 +11,89 @@
 #include <protorpc/rpc_server.h>
 #include <protorpc/rpc_client.h>
 
-class ArithService: public service::ArithService {
- public:
-  inline ArithService() {}
-  virtual ~ArithService() {}
+struct ArithService: public service::ArithService {
+	inline ArithService() {}
+	virtual ~ArithService() {}
 
-  virtual const ::protorpc::Error add(
-    const ::service::ArithRequest* args,
-    ::service::ArithResponse* reply
-  ) {
-    reply->set_c(args->a() + args->b());
-    printf("ArithService.Add: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
-      args->a(), args->b(), reply->c(), ""
-    );
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error mul(
-    const ::service::ArithRequest* args,
-    ::service::ArithResponse* reply
-  ) {
-    reply->set_c(args->a() * args->b());
-    printf("ArithService.Mul: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
-      args->a(), args->b(), reply->c(), ""
-    );
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error div(
-    const ::service::ArithRequest* args,
-    ::service::ArithResponse* reply
-  ) {
-    if(args->b() == 0) {
-      printf("ArithService.Div: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
-        args->a(), args->b(), reply->c(), "divide by zero"
-      );
-      return ::protorpc::Error::New("divide by zero");
-    }
-    reply->set_c(args->a() / args->b());
-    printf("ArithService.Div: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
-      args->a(), args->b(), reply->c(), ""
-    );
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error error(
-    const ::service::ArithRequest* args,
-    ::service::ArithResponse* reply
-  ) {
-    printf("ArithService.Error: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
-      args->a(), args->b(), reply->c(), "ArithError"
-    );
-    return ::protorpc::Error::New("ArithError");
-  }
+	virtual const ::protorpc::Error add(
+		const ::service::ArithRequest* args,
+		::service::ArithResponse* reply
+	) {
+		reply->set_c(args->a() + args->b());
+		printf("ArithService.Add: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
+			args->a(), args->b(), reply->c(), ""
+		);
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error mul(
+		const ::service::ArithRequest* args,
+		::service::ArithResponse* reply
+	) {
+		reply->set_c(args->a() * args->b());
+		printf("ArithService.Mul: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
+			args->a(), args->b(), reply->c(), ""
+		);
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error div(
+		const ::service::ArithRequest* args,
+		::service::ArithResponse* reply
+	) {
+		if(args->b() == 0) {
+			printf("ArithService.Div: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
+				args->a(), args->b(), reply->c(), "divide by zero"
+			);
+			return ::protorpc::Error::New("divide by zero");
+		}
+		reply->set_c(args->a() / args->b());
+		printf("ArithService.Div: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
+			args->a(), args->b(), reply->c(), ""
+		);
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error error(
+		const ::service::ArithRequest* args,
+		::service::ArithResponse* reply
+	) {
+		printf("ArithService.Error: args = a:%d b:%d, reply = c:%d, err = \"%s\"\n",
+			args->a(), args->b(), reply->c(), "ArithError"
+		);
+		return ::protorpc::Error::New("ArithError");
+	}
 };
 
-class EchoService: public service::EchoService {
- public:
-  inline EchoService() {}
-  virtual ~EchoService() {}
+struct EchoService: public service::EchoService {
+	inline EchoService() {}
+	virtual ~EchoService() {}
 
-  virtual const ::protorpc::Error Echo(
-    const ::service::EchoRequest* args,
-    ::service::EchoResponse* reply
-  ) {
-    reply->set_msg(args->msg());
-    printf("EchoService.Echo: args = msg:\"%s\", reply = msg:\"%s\", err = \"%s\"\n",
-      args->msg().c_str(), reply->msg().c_str(), ""
-    );
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error EchoTwice(
-    const ::service::EchoRequest* args,
-    ::service::EchoResponse* reply
-  ) {
-    reply->set_msg(args->msg() + args->msg());
-    printf("EchoService.EchoTwice: args = msg:\"%s\", reply = msg:\"%s\", err = \"%s\"\n",
-      args->msg().c_str(), reply->msg().c_str(), ""
-    );
-    return ::protorpc::Error::Nil();
-  }
+	virtual const ::protorpc::Error Echo(
+		const ::service::EchoRequest* args,
+		::service::EchoResponse* reply
+	) {
+		reply->set_msg(args->msg());
+		printf("EchoService.Echo: args = msg:\"%s\", reply = msg:\"%s\", err = \"%s\"\n",
+			args->msg().c_str(), reply->msg().c_str(), ""
+		);
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error EchoTwice(
+		const ::service::EchoRequest* args,
+		::service::EchoResponse* reply
+	) {
+		reply->set_msg(args->msg() + args->msg());
+		printf("EchoService.EchoTwice: args = msg:\"%s\", reply = msg:\"%s\", err = \"%s\"\n",
+			args->msg().c_str(), reply->msg().c_str(), ""
+		);
+		return ::protorpc::Error::Nil();
+	}
 };
 
 int main() {
-  ::protorpc::Server server;
+	::protorpc::Server server;
 
-  server.AddService(new ArithService, true);
-  server.AddService(new EchoService, true);
+	server.AddService(new ArithService, true);
+	server.AddService(new EchoService, true);
 
-  server.BindAndServe(1234);
-  return 0;
+	server.BindAndServe(1234);
+	return 0;
 }
--- examples/rpctest.cc
+++ examples/rpctest.cc
@@ -13,177 +13,175 @@
 #include "./service.pb/arith.protorpc.golden.h"
 #include "./service.pb/echo.protorpc.golden.h"
 
-class ArithService: public service::ArithService {
- public:
-  inline ArithService() {}
-  virtual ~ArithService() {}
+struct ArithService: public service::ArithService {
+	inline ArithService() {}
+	virtual ~ArithService() {}
 
-  virtual const ::protorpc::Error add(
-    const ::service::ArithRequest* request,
-    ::service::ArithResponse* response
-  ) {
-    response->set_c(request->a() + request->b());
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error mul(
-    const ::service::ArithRequest* request,
-    ::service::ArithResponse* response
-  ) {
-    response->set_c(request->a() * request->b());
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error div(
-    const ::service::ArithRequest* request,
-    ::service::ArithResponse* response
-  ) {
-    if(request->b() == 0) {
-      return ::protorpc::Error::New("divide by zero");
-    }
-    response->set_c(request->a() / request->b());
-    return ::protorpc::Error::Nil();
-  }
-  virtual const ::protorpc::Error error(
-    const ::service::ArithRequest* request,
-    ::service::ArithResponse* response
-  ) {
-    return ::protorpc::Error::New("ArithError");
-  }
+	virtual const ::protorpc::Error add(
+		const ::service::ArithRequest* request,
+		::service::ArithResponse* response
+	) {
+		response->set_c(request->a() + request->b());
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error mul(
+		const ::service::ArithRequest* request,
+		::service::ArithResponse* response
+	) {
+		response->set_c(request->a() * request->b());
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error div(
+		const ::service::ArithRequest* request,
+		::service::ArithResponse* response
+	) {
+		if(request->b() == 0) {
+			return ::protorpc::Error::New("divide by zero");
+		}
+		response->set_c(request->a() / request->b());
+		return ::protorpc::Error::Nil();
+	}
+	virtual const ::protorpc::Error error(
+		const ::service::ArithRequest* request,
+		::service::ArithResponse* response
+	) {
+		return ::protorpc::Error::New("ArithError");
+	}
 };
 
-class EchoService: public service::EchoService {
- public:
-  inline EchoService() {}
-  virtual ~EchoService() {}
+struct EchoService: public service::EchoService {
+	inline EchoService() {}
+	virtual ~EchoService() {}
 
-  virtual const ::protorpc::Error Echo(
-    const ::service::EchoRequest* request,
-    ::service::EchoResponse* response
-  ) {
-    response->set_msg(request->msg());
-    return ::protorpc::Error::Nil();
-  }
+	virtual const ::protorpc::Error Echo(
+		const ::service::EchoRequest* request,
+		::service::EchoResponse* response
+	) {
+		response->set_msg(request->msg());
+		return ::protorpc::Error::Nil();
+	}
 };
 
 int main(int argc, char* argv[]) {
-  ::protorpc::Server client;
+	::protorpc::Server client;
 
-  client.AddService(new ArithService, true);
-  client.AddService(new EchoService, true);
+	client.AddService(new ArithService, true);
+	client.AddService(new EchoService, true);
 
-  service::ArithService::Stub arithStub(&client);
-  service::EchoService::Stub echoStub(&client);
+	service::ArithService::Stub arithStub(&client);
+	service::EchoService::Stub echoStub(&client);
 
-  ::service::ArithRequest arithArgs;
-  ::service::ArithResponse arithReply;
-  ::service::EchoRequest echoArgs;
-  ::service::EchoResponse echoReply;
-  ::protorpc::Error err;
+	::service::ArithRequest arithArgs;
+	::service::ArithResponse arithReply;
+	::service::EchoRequest echoArgs;
+	::service::EchoResponse echoReply;
+	::protorpc::Error err;
 
-  // Test CamelCase
-  struct { const char *in, *out; } methods[] = {
-    { ".add", ".Add" },
-    { "a.add", "A.Add" },
-    { "abc_aa_bb", "AbcAaBb" },
-    { "", "" },
-  };
-  for(int i = 0; i < sizeof(methods)/sizeof(methods[0]); i++) {
-    auto s = ::protorpc::Service::CamelCase(methods[i].in);
-    if(s != methods[i].out) {
-      fprintf(stderr, "Service::CamelCase: expected = \"%s\", got = \"%s\"\n",
-        methods[i].out, s.c_str()
-      );
-      return -1;
-    }
-  }
+	// Test CamelCase
+	struct { const char *in, *out; } methods[] = {
+		{ ".add", ".Add" },
+		{ "a.add", "A.Add" },
+		{ "abc_aa_bb", "AbcAaBb" },
+		{ "", "" },
+	};
+	for(int i = 0; i < sizeof(methods)/sizeof(methods[0]); i++) {
+		auto s = ::protorpc::Service::CamelCase(methods[i].in);
+		if(s != methods[i].out) {
+			fprintf(stderr, "Service::CamelCase: expected = \"%s\", got = \"%s\"\n",
+				methods[i].out, s.c_str()
+			);
+			return -1;
+		}
+	}
 
-  // EchoService.add
-  arithArgs.set_a(1);
-  arithArgs.set_b(2);
-  err = arithStub.add(&arithArgs, &arithReply);
-  if(!err.IsNil()) {
-    fprintf(stderr, "arithStub.add: %s\n", err.String().c_str());
-    return -1;
-  }
-  if(arithReply.c() != 3) {
-    fprintf(stderr, "arithStub.add: expected = %d, got = %d\n", 3, arithReply.c());
-    return -1;
-  }
+	// EchoService.add
+	arithArgs.set_a(1);
+	arithArgs.set_b(2);
+	err = arithStub.add(&arithArgs, &arithReply);
+	if(!err.IsNil()) {
+		fprintf(stderr, "arithStub.add: %s\n", err.String().c_str());
+		return -1;
+	}
+	if(arithReply.c() != 3) {
+		fprintf(stderr, "arithStub.add: expected = %d, got = %d\n", 3, arithReply.c());
+		return -1;
+	}
 
-  // EchoService.mul
-  arithArgs.set_a(3);
-  arithArgs.set_b(4);
-  err = arithStub.mul(&arithArgs, &arithReply);
-  if(!err.IsNil()) {
-    fprintf(stderr, "arithStub.mul: %s\n", err.String().c_str());
-    return -1;
-  }
-  if(arithReply.c() != 12) {
-    fprintf(stderr, "arithStub.mul: expected = %d, got = %d\n", 12, arithReply.c());
-    return -1;
-  }
+	// EchoService.mul
+	arithArgs.set_a(3);
+	arithArgs.set_b(4);
+	err = arithStub.mul(&arithArgs, &arithReply);
+	if(!err.IsNil()) {
+		fprintf(stderr, "arithStub.mul: %s\n", err.String().c_str());
+		return -1;
+	}
+	if(arithReply.c() != 12) {
+		fprintf(stderr, "arithStub.mul: expected = %d, got = %d\n", 12, arithReply.c());
+		return -1;
+	}
 
-  // EchoService.div
-  arithArgs.set_a(13);
-  arithArgs.set_b(5);
-  err = arithStub.div(&arithArgs, &arithReply);
-  if(!err.IsNil()) {
-    fprintf(stderr, "arithStub.div: %s\n", err.String().c_str());
-    return -1;
-  }
-  if(arithReply.c() != 2) {
-    fprintf(stderr, "arithStub.div: expected = %d, got = %d\n", 2, arithReply.c());
-    return -1;
-  }
+	// EchoService.div
+	arithArgs.set_a(13);
+	arithArgs.set_b(5);
+	err = arithStub.div(&arithArgs, &arithReply);
+	if(!err.IsNil()) {
+		fprintf(stderr, "arithStub.div: %s\n", err.String().c_str());
+		return -1;
+	}
+	if(arithReply.c() != 2) {
+		fprintf(stderr, "arithStub.div: expected = %d, got = %d\n", 2, arithReply.c());
+		return -1;
+	}
 
-  // EchoService.div: divide by zero
-  arithArgs.set_a(1);
-  arithArgs.set_b(0);
-  err = arithStub.div(&arithArgs, &arithReply);
-  if(err.IsNil() || err.String() != "divide by zero") {
-    fprintf(stderr, "arithStub.div: expected = %s, got = %s\n",
-      "divide by zero", err.String().c_str()
-    );
-    return -1;
-  }
+	// EchoService.div: divide by zero
+	arithArgs.set_a(1);
+	arithArgs.set_b(0);
+	err = arithStub.div(&arithArgs, &arithReply);
+	if(err.IsNil() || err.String() != "divide by zero") {
+		fprintf(stderr, "arithStub.div: expected = %s, got = %s\n",
+			"divide by zero", err.String().c_str()
+		);
+		return -1;
+	}
 
-  // EchoService.error
-  err = arithStub.error(&arithArgs, &arithReply);
-  if(err.IsNil() || err.String() != "ArithError") {
-    fprintf(stderr, "arithStub.error: expected = \"%s\", got = \"%s\"\n",
-      "ArithError", err.String().c_str()
-    );
-    return -1;
-  }
+	// EchoService.error
+	err = arithStub.error(&arithArgs, &arithReply);
+	if(err.IsNil() || err.String() != "ArithError") {
+		fprintf(stderr, "arithStub.error: expected = \"%s\", got = \"%s\"\n",
+			"ArithError", err.String().c_str()
+		);
+		return -1;
+	}
 
-  // EchoService.Echo
-  echoArgs.set_msg("Hello Protobuf-RPC!");
-  err = echoStub.Echo(&echoArgs, &echoReply);
-  if(!err.IsNil()) {
-    fprintf(stderr, "echoStub.Echo: %s\n", err.String().c_str());
-    return -1;
-  }
-  if(echoReply.msg() != echoArgs.msg()) {
-    fprintf(stderr, "echoStub.Echo: expected = \"%s\", got = \"%s\"\n",
-      echoArgs.msg().c_str(), echoReply.msg().c_str()
-    );
-    return -1;
-  }
+	// EchoService.Echo
+	echoArgs.set_msg("Hello Protobuf-RPC!");
+	err = echoStub.Echo(&echoArgs, &echoReply);
+	if(!err.IsNil()) {
+		fprintf(stderr, "echoStub.Echo: %s\n", err.String().c_str());
+		return -1;
+	}
+	if(echoReply.msg() != echoArgs.msg()) {
+		fprintf(stderr, "echoStub.Echo: expected = \"%s\", got = \"%s\"\n",
+			echoArgs.msg().c_str(), echoReply.msg().c_str()
+		);
+		return -1;
+	}
 
-  // EchoService.Echo: Use Client
-  echoArgs.set_msg("Hello Protobuf-RPC!");
-  err = client.CallMethod("EchoService.Echo", &echoArgs, &echoReply);
-  if(!err.IsNil()) {
-    fprintf(stderr, "EchoService.Echo: %s\n", err.String().c_str());
-    return -1;
-  }
-  if(echoReply.msg() != echoArgs.msg()) {
-    fprintf(stderr, "EchoService.Echo: expected = \"%s\", got = \"%s\"\n",
-      echoArgs.msg().c_str(), echoReply.msg().c_str()
-    );
-    return -1;
-  }
+	// EchoService.Echo: Use Client
+	echoArgs.set_msg("Hello Protobuf-RPC!");
+	err = client.CallMethod("EchoService.Echo", &echoArgs, &echoReply);
+	if(!err.IsNil()) {
+		fprintf(stderr, "EchoService.Echo: %s\n", err.String().c_str());
+		return -1;
+	}
+	if(echoReply.msg() != echoArgs.msg()) {
+		fprintf(stderr, "EchoService.Echo: expected = \"%s\", got = \"%s\"\n",
+			echoArgs.msg().c_str(), echoReply.msg().c_str()
+		);
+		return -1;
+	}
 
-  printf("RpcTest Done.\n");
-  return 0;
+	printf("RpcTest Done.\n");
+	return 0;
 }
 
