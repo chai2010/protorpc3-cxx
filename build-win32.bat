c3db4b257985c4d559c64956b8c3aabe13a21cb1
chai2010 <chaishushan@gmail.com>
2016-03-04 15:07:37 +0800

    move protobuf to ${PROJECT_SOURCE_DIR}/3rdparty

--- build-win32.bat
+++ build-win32.bat
@@ -9,6 +9,13 @@ cd %~dp0
 :: ----------------------------------------------------------------------------
 :: Setup MSVC
 
+:: VS2015
+if not "x%VS140COMNTOOLS%" == "x" (
+	echo Setup VS2015 Win32 ...
+	call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"
+	goto build
+)
+
 :: VS2013
 if not "x%VS120COMNTOOLS%" == "x" (
 	echo Setup VS2013 Win32 ...
--- build-win64.bat
+++ build-win64.bat
@@ -9,10 +9,17 @@ cd %~dp0
 :: ----------------------------------------------------------------------------
 :: Setup MSVC
 
-:: VS2010
-if not "x%VS100COMNTOOLS%" == "x" (
-	echo Setup VS2010 Win64 ...
-	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+:: VS2015
+if not "x%VS140COMNTOOLS%" == "x" (
+	echo Setup VS2015 Win64 ...
+	call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+	goto build
+)
+
+:: VS2013
+if not "x%VS120COMNTOOLS%" == "x" (
+	echo Setup VS2013 Win64 ...
+	call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 	goto build
 )
 
@@ -23,10 +30,10 @@ if not "x%VS110COMNTOOLS%" == "x" (
 	goto build
 )
 
-:: VS2013
-if not "x%VS120COMNTOOLS%" == "x" (
-	echo Setup VS2013 Win64 ...
-	call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+:: VS2010
+if not "x%VS100COMNTOOLS%" == "x" (
+	echo Setup VS2010 Win64 ...
+	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 	goto build
 )
 
